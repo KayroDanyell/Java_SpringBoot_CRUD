adbebbae3cbe57a6738aa7a033a58aef5df2fc7c
Kayro <kayrodanyell@gmail.com>
2022-10-08 18:40:53 -0300

    Implementando Busca de Produto por Id e Delete de Produto.

--- src/main/java/com/example/exerciciossb/controllers/ProdutoController.java
+++ src/main/java/com/example/exerciciossb/controllers/ProdutoController.java
@@ -5,6 +5,9 @@ import com.example.exerciciossb.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.Optional;
+
 @RestController
 @RequestMapping("api/produtos")
 public class ProdutoController {
@@ -13,8 +16,24 @@ public class ProdutoController {
     private ProdutoRepository produtoReposit;
 
     @PostMapping
-    public @ResponseBody Produto newProd(Produto prod){
+    public @ResponseBody Produto newProd(@Valid Produto prod){
         produtoReposit.save(prod);
         return prod;
+    }
+
+    @GetMapping
+    public Iterable<Produto> allProds(){
+        return produtoReposit.findAll();
+
+    }
+    @GetMapping(path = "/{id}")
+    public Optional<Produto> getProd(@PathVariable int id){
+        Optional<Produto> a = produtoReposit.findById(id);
+        return a;
+    }
+    @DeleteMapping
+    public int deleteProd(@RequestParam int id){
+        produtoReposit.deleteById(id);
+        return id;
     }
 }
--- src/main/java/com/example/exerciciossb/model/entities/Produto.java
+++ src/main/java/com/example/exerciciossb/model/entities/Produto.java
@@ -4,10 +4,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 public class Produto {
-
+    public Produto(){}
     public Produto(String nome,double preco,double desconto){
         this.nome = nome;
         this.preco = preco;
@@ -16,10 +19,12 @@ public class Produto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @NotBlank
     private String nome;
-
+    @Min(1)
     private double preco;
-
+    @Min(0)
+    @Max(1)
     private double desconto;
 
     public double getPreco() {
